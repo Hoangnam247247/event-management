3ca36f5d36b0d9d7921e6b2cc75cf38324f63cad
Din2609 <tqv.2609@gmail.com>
2025-12-20 02:07:50 +0700

    Update

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://eventuser:event123@localhost/event247"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://eventuser:event123@localhost/event247"
+)
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
